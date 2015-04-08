4ee6de70e14262ea6b850e7d7fc17d7c2c9d7b12
Stanislav Stipl <stanislav.stipl@gmail.com>
2015-04-08 18:52:49 +0200

    end

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,9 @@ class IndexController < ActionController::Base
   def home
     if user_signed_in?
       redirect_to controller: 'people', action: 'index'
+    else
+      render :home, layout: 'application'
     end
-    render :home, layout: 'application'
+    
   end
 end

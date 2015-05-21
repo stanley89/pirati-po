a46e5e548fbf11968d7556bb66ed3759552e5aa0
Roj007 <rojcze@gmail.com>
2015-05-22 01:11:15 +0200

    form mentor_id OK

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521205247) do
+ActiveRecord::Schema.define(version: 20150521222055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150521205247) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "mentor_id"
+    t.boolean  "is_mentor"
   end
 
   add_index "people", ["mentor_id"], name: "index_people_on_mentor_id", using: :btree

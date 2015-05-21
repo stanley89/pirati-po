af1fca3ae48f8051632ced9a7d4f81f3d46a96c2
Roj007 <rojcze@gmail.com>
2015-05-22 00:03:00 +0200

    sloupec mentor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415134446) do
+ActiveRecord::Schema.define(version: 20150521205247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,10 @@ ActiveRecord::Schema.define(version: 20150415134446) do
     t.string   "jabber"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "mentor_id"
   end
+
+  add_index "people", ["mentor_id"], name: "index_people_on_mentor_id", using: :btree
 
   create_table "people_skills", force: true do |t|
     t.integer  "person_id"

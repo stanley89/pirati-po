da1c4f4095bae8ef00e1e5e339e03592e140b649
Stanislav Stipl <stanislav.stipl@gmail.com>
2015-05-24 18:07:58 +0200

    people_skill

--- app/models/people_skill.rb
+++ app/models/people_skill.rb
@@ -1,4 +1,7 @@
 class PeopleSkill < ActiveRecord::Base
   belongs_to :person
   belongs_to :skill
+
+  validates_presence_of :level
+  validates :level, :inclusion => {:in => [1,2]}
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,9 @@ class Person < ActiveRecord::Base
   has_many :people_skills, foreign_key: :person_id
   has_many :skills, through: :people_skills
 
+  validates_presence_of :name, :message => "Nemas jmeno"
+
+
   def not_my_skills
     my_skills_ids = skills.ids
     Skill.where.not(id:my_skills_ids)

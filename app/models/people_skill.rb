04a23b20e3409f2a07220ff20c163b8c127b3d5b
Roj007 <rojcze@gmail.com>
2015-06-17 22:22:20 +0200

    validace people_skill

--- app/models/people_skill.rb
+++ app/models/people_skill.rb
@@ -3,7 +3,7 @@ class PeopleSkill < ActiveRecord::Base
   belongs_to :skill
 
   validates_presence_of :level
-  validates :level, :inclusion => {:in => [1,2]}
+  validates :level, :inclusion => {:in => [1,3]}
   validates_presence_of :favor
-  validates :favor, :inclusion => {:in => [1,2]}
+  validates :favor, :inclusion => {:in => [1,3]}
 end
